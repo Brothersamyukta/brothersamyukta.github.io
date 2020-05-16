ae67b0004e49d2dbec34f305afc1389b5d893fdc
Brothersamyukta <rai19003@byui.edu>
2020-05-16 15:03:04 -0600

    fixedbuglesson4 assignment

--- lesson4/hello.js
+++ lesson4/hello.js
@@ -1,3 +1,35 @@
 function toggleMenu () {
     (document.getElementById("primaryNav").classList.toggle("hide"));
 }
+let daysNames = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
+let months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+  
+let date = new Date();
+let dayName = daysNames[date.getDay()];
+let monthName = months[date.getMonth()];
+let year = date.getFullYear();
+let fullDate = dayName + ', '+ date.getDate() +" " + monthName + " "+ year;
+  
+document.getElementById('lastUpdated').textContent = fullDate;
+  
