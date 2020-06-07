3c34930d5d37107f65d19e1d1af0a14ca975dc3f
Brothersamyukta <rai19003@byui.edu>
2020-06-06 20:34:09 -0600

    lesson7 assignment

--- lesson7/lazy.js
+++ lesson7/lazy.js
@@ -1,30 +1,63 @@
-const images= document.querySelectorAll("img[data-src]");
+// const images= document.querySelectorAll("img[data-src]");
+
+// function preloadImage(img) {
+//     const src = img.getAttribute("data-src");
+//     if(!src) {
+//         return;
+//     }
+//     img.src = src;
+// }
+// const imgOptions = {
+//     threshold: 1,
+//     rootMargin: "0px 0px 200px 0px"
+// };
+
+// const imgObserver= new IntersectionObserver((entries, imgObserver) => {
+//     entries.forEach(entry => {
+//         if (!entry.isIntersecting) {
+//             return;
+//         } else {
+//             preloadImage(entry.target);
+//             imgObserver.unobserve(entry.target);
+//         }
+//     })
+
+// }, imgOptions);
+
+
+// images.forEach(image => {
+//     imgObserver.observe(image);
+// });
+
+
+const imagesToLoad = document.querySelectorAll("img[data-src]");
 
 function preloadImage(img) {
-    const src = img.getAttribute("data-src");
-    if(!src) {
-        return;
-    }
-    img.src = src;
+  const src= img.getAttribute("data-src");
+  if(!src) {
+    return;
+  }
+  img.src = src;
 }
-const imgOptions = {
-    threshold: 0,
-    rootMargin: "0px 0px 300px 0px"
+
+const imageOptions = {
+  threshold:1,
+  rootMargin: "0px 0px 20px 0px"
 };
 
-const imgObserver= new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            preloadImage(entry.target);
-            imgObserver.unobserve(entry.target);
-        }
-    })
+const imgObserver = new IntersectionObserver((entries,imgObserver) => {
+  entries.forEach(entry => {
 
-}, imgOptions);
+    if(!entry.isIntersecting) {
+     return;
+   }
+    else {
+      preloadImage(entry.target);
+      imgObserver.unobserve(entry.target);
+    }
+  });
+},imageOptions);
 
-
-images.forEach(image => {
-    imgObserver.observe(image);
+imagesToLoad.forEach(image =>  {
+  imgObserver.observe(image);
 });
