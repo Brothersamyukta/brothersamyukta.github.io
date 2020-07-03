d815c7c9adb121075c5917dab74df2976187783f
Brothersamyukta <rai19003@byui.edu>
2020-07-03 16:34:07 -0600

    Assignment fixed

---
+++ lesson3/app.js
@@ -0,0 +1,6 @@
+// //toggle menu
+function toggleMenu () {
+    (document.getElementsByClassName("navigation")[0].classList.toggle("responsive"));
+}
+ 
+document.getElementById('lastUpdated').innerHTML =document.lastModified; 
